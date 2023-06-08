36b1be6341a9fae5279d9a5353f5c7069913fcbb
Ernesto Anaya <ernestex@gmail.com>
2023-06-08 11:07:45 -0500

    cliente-soap

---
+++ cliente-soap/src/test/java/edu/idat/dsw1/clientesoap/ClienteSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.idat.dsw1.clientesoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
